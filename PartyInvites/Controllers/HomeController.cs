0d43809e0e38a15310f2a2dc61362606db3e8baa
Bobby Morehead <bobby.morehead@outlook.com>
2018-09-23 18:41:37 -0400

    Final

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -11,7 +12,37 @@ namespace PartyInvites.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            string timeOfDay;
+            if (hour < 12)
+                timeOfDay = "Good Morning";
+            else if (hour < 18)
+                timeOfDay = "Good Afternoon";
+            else
+                timeOfDay = "Good Evening";
+            ViewBag.Greetings = timeOfDay;
             return View();
         }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Email response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+        }
+
     }
 }
